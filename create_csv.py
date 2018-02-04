494abccfee2afe901e01f48e62d35ffad12861d8
Sumit Saha <sumit.saha666@gmail.com>
2018-02-04 19:45:50 +0530

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -1,12 +1,21 @@
 import sqlite3
 
+# 
+# connecting to example database
+#
 database = "truemarble.sqlite"
 conn = sqlite3.connect(database)
 curs = conn.cursor()
 
+#
+# get table names from example database
+#
 curs.execute("SELECT tbl_name FROM sqlite_master WHERE type='table';")
 tables = curs.fetchall()
 
+#
+# CSV file generation 
+#
 for i in tables:
     print('Creating CSV for: ',i[0])
     curs.execute("SELECT * FROM %s;" % i[0])
@@ -14,9 +23,9 @@ for i in tables:
 
     for row in ex :
         list = []
-        for j in row:
-            value = str(j)
+        for sub_value in row:
+            value = str(sub_value)
             list.append(value)
-        file = open('./database/%s.csv'% i[0], 'a')
+        file = open('./database/%s.csv'% i[0], 'a') #opens respective CSV file in append mode
         ex = csv.writer(file)
         ex.writerow(list)
