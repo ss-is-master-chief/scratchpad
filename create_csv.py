70f46d64b6cf851cd87cd9695d9799f441b5393a
Sumit Saha <sumit.saha666@gmail.com>
2018-02-05 18:31:04 +0530

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -1,42 +1,54 @@
-import sqlite3
-import os
-import csv
+import sqlite3, os, csv, glob
 
 #
-# data to be imported fron current working directory
+# finds all necessary .tif images from current working directory
 #
-image = "NLCD2011_LC_N39W105.tif"
-database = "truemarble.sqlite"
+files = []
+for file in glob.glob("*.tif"):
+    files.append(file)
+    
+count_files = len(files)
 
-conn = sqlite3.connect(database)
-curs = conn.cursor()
+#
+# data to be imported
+#
+
+for file in files:                          # iterates through all images and creates separate datasets
+    
+    file_name = file.strip(".tif")
+    database = ("%s.sqlite" % file_name)
+    conn = sqlite3.connect(database)
+    curs = conn.cursor()
 
 #
 # setting up metadata
 #
-curs.execute("SELECT load_extension('mod_spatialite');") # OperationalError: not authorized
-curs.execute("SELECT InitSpatialMetaData(1);") # if Line 17 removed : Error - no such function 'InitSpatialMetaData()'
-
-curs.fetchall()
+    curs.execute("SELECT InitSpatialMetadata()")
+    curs.fetchall()
 
 #
 # loading raster data from objects
 #
-os.system("rasterlite_load -d truemarble.sqlite -T TrueMarble -D . -t")
-os.system("rasterlite_load -d truemarble.sqlite -T TrueMarble -D . -v")
+    os.system("rasterlite_load -d %s.sqlite -T %s -D . -t" % (file_name,file_name))
+    os.system("rasterlite_load -d %s.sqlite -T %s -D . -v" % (file_name,file_name))
 
-curs.execute("SELECT tbl_name FROM sqlite_master WHERE type='table';")
-tables = curs.fetchall()
-
-for i in tables:
-    print('Creating CSV for: ',i[0])
-    curs.execute("SELECT * FROM %s;" % i[0])
-    ex = curs.fetchall()
-    for row in ex :
-        list = []
-        for j in row:
-            value = str(j)
-            list.append(value)    
-        file = open('./%s.csv'% i[0], 'a')
-        ex = csv.writer(file)
-        ex.writerow(list)
+    curs.execute("SELECT tbl_name FROM sqlite_master WHERE type='table';")
+    tables = curs.fetchall()
+    
+    os.system("mkdir %s_files" % file_name)
+    
+#
+# generating CSVs
+#
+    for i in tables:
+        print('Creating CSV for: ',i[0])
+        curs.execute("SELECT * FROM %s;" % i[0])
+        ex = curs.fetchall()
+        for row in ex :
+            list = []
+            for j in row:
+                value = str(j)
+                list.append(value)    
+            file_data = open('.%s_files/%s.csv' % (file_name,i[0]), 'a')
+            ex = csv.writer(file_data)
+            ex.writerow(list)
