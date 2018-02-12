7c0ee5ec60e0d998a8a70680ad561e9bc941b034
Sumit Saha <sumit.saha666@gmail.com>
2018-02-12 22:28:54 +0530

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -6,7 +6,7 @@ import sqlite3, os, csv, glob
 files = []
 for file in glob.glob("*.tif"):
     files.append(file)
-    
+
 count_files = len(files)
 
 #
@@ -14,9 +14,9 @@ count_files = len(files)
 #
 
 for file in files:                          # iterates through all images and creates separate datasets
-    
+
     file_name = file.strip(".tif")
-    database = ("%s.sqlite" % file_name)
+    database = ("{}.sqlite".format(file_name))
     conn = sqlite3.connect(database)
     curs = conn.cursor()
 
@@ -29,26 +29,26 @@ for file in files:                          # iterates through all images and cr
 #
 # loading raster data from objects
 #
-    os.system("rasterlite_load -d %s.sqlite -T %s -D . -t" % (file_name,file_name))
-    os.system("rasterlite_load -d %s.sqlite -T %s -D . -v" % (file_name,file_name))
+    os.system("rasterlite_load -d {}.sqlite -T {} -D . -t".format(file_name,file_name))
+    os.system("rasterlite_load -d {}.sqlite -T {} -D . -v".format(file_name,file_name))
 
     curs.execute("SELECT tbl_name FROM sqlite_master WHERE type='table';")
     tables = curs.fetchall()
-    
-    os.system("mkdir %s_files" % file_name)
-    
+
+    os.system("mkdir {}_files".format(file_name))
+
 #
 # generating CSVs
 #
     for i in tables:
         print('Creating CSV for: ',i[0])
-        curs.execute("SELECT * FROM %s;" % i[0])
+        curs.execute("SELECT * FROM {};".format(i[0]))
         ex = curs.fetchall()
         for row in ex :
             list = []
             for j in row:
                 value = str(j)
-                list.append(value)    
-            file_data = open('.%s_files/%s.csv' % (file_name,i[0]), 'a')
+                list.append(value)
+            file_data = open('.{}_files/{}.csv'.format(file_name,i[0]), 'a')
             ex = csv.writer(file_data)
             ex.writerow(list)
