a96995b2f99a3da6cbee0ea09c3a5f3e78bb6304
Sumit Saha <sumit.saha666@gmail.com>
2018-02-13 19:59:58 +0530

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -1,45 +1,20 @@
 import sqlite3, os, csv, glob
 
-#
-# finds all necessary .tif images from current working directory
-#
-files = []
-for file in glob.glob("*.tif"):
-    files.append(file)
 
-count_files = len(files)
+def get_tif_files():
 
-#
-# data to be imported
-#
+    files = []
+    for file in glob.glob("*.tif"):
+        files.append(file)
+    return files
 
-for file in files:                          # iterates through all images and creates separate datasets
+def load_data(file_name):
 
-    file_name = file.strip(".tif")
-    database = ("{}.sqlite".format(file_name))
-    conn = sqlite3.connect(database)
-    curs = conn.cursor()
-
-#
-# setting up metadata
-#
-    curs.execute("SELECT InitSpatialMetadata()")
-    curs.fetchall()
-
-#
-# loading raster data from objects
-#
     os.system("rasterlite_load -d {}.sqlite -T {} -D . -t".format(file_name,file_name))
     os.system("rasterlite_load -d {}.sqlite -T {} -D . -v".format(file_name,file_name))
 
-    curs.execute("SELECT tbl_name FROM sqlite_master WHERE type='table';")
-    tables = curs.fetchall()
+def create_csv(tables, file_name, curs):
 
-    os.system("mkdir {}_files".format(file_name))
-
-#
-# generating CSVs
-#
     for i in tables:
         print('Creating CSV for: ',i[0])
         curs.execute("SELECT * FROM {};".format(i[0]))
@@ -52,3 +27,26 @@ for file in files:                          # iterates through all images and cr
             file_data = open('.{}_files/{}.csv'.format(file_name,i[0]), 'a')
             ex = csv.writer(file_data)
             ex.writerow(list)
+
+def create_db():
+
+    files = get_tif_files()
+
+    for file in files:
+
+        file_name = file.strip(".tif")
+        database = ("{}.sqlite".format(file_name))
+        conn = sqlite3.connect(database)
+        curs = conn.cursor()
+
+        curs.execute("SELECT InitSpatialMetadata()")
+        curs.fetchall()
+
+        load_data(file_name)
+
+        curs.execute("SELECT tbl_name FROM sqlite_master WHERE type='table';")
+        tables = curs.fetchall()
+
+        os.system("mkdir {}_files".format(file_name))
+
+        create_csv(tables, file_name, curs)
