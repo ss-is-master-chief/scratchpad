da3fe69c73c72bf9adb38fced9ac6da348c23bab
ss-is-master-chief <sumit.saha666@gmail.com>
2018-03-12 00:41:11 +0530

    refactoring

---
+++ Raster.py
@@ -0,0 +1,110 @@
+import glob
+import linecache
+import pandas as pd
+import os
+from tqdm import tqdm
+
+# Resolve link : https://gis.stackexchange.com/questions/233654/install-gdal-python-binding-on-mac
+
+'''Add proper path variables as follows'''
+sys.path.insert(0,'/Library/Frameworks/GDAL.framework/Versions/2.2/Python/3.6/site-packages')
+
+
+'''Check if ogr, osr, gdal libraries are imported from osgeo'''
+
+try:
+    from osgeo import ogr, osr, gdal
+except:
+    sys.exit('ERROR: cannot find GDAL/OGR modules')
+
+'''Check version of GDAL'''
+
+version_num = int(gdal.VersionInfo('VERSION_NUM'))
+
+if version_num < 1100000:
+    sys.exit('ERROR: Python bindings of GDAL 1.10 or later required')
+
+directory = str(os.system('pwd'))
+
+class Raster:
+
+    raster_extensions = ['.tif']
+
+    def __init__(self):
+
+        files = get_grid_files()
+
+    def get_grid_files():
+
+        files_grid = []
+        for file in glob.glob("*.asc"):
+            files_grid.append(file)
+        return files_grid
+
+    '''Converting asc files into CSV'''
+
+    def install_grid(file_name):
+
+        file_name = file_name.strip('.asc')
+
+        file_type = 'asc'
+        convert_to = 'csv'
+
+        '''Will overwrite/update file if it already exists'''
+
+        if(os.path.exists('{}.{}'.format(file_name,convert_to))):
+            print('{}.{} already exists. Updating..'.format(file_name,convert_to))
+            open('{}.{}'.format(file_name,convert_to), 'w').close()
+        else:
+            print('Installing {}.{}'.format(file_name,convert_to))
+
+
+        file = open("{}.{}".format(file_name,convert_to),'a')
+
+        '''Fetching meta-data for file'''
+
+        n_col = linecache.getline('{}.{}'.format(file_name,file_type),1).split(' ')
+        n_col = n_col[1]
+
+        n_row = linecache.getline('{}.{}'.format(file_name,file_type),2).split(' ')
+        n_row = int(n_row[1])
+
+        xllcenter = linecache.getline('{}.{}'.format(file_name,file_type),3).split(' ')
+        xllcenter = xllcenter[1]
+
+        yllcenter = linecache.getline('{}.{}'.format(file_name,file_type),4).split(' ')
+        yllcenter = yllcenter[1]
+
+        cellsize = linecache.getline('{}.{}'.format(file_name,file_type),5).split(' ')
+        cellsize = cellsize[1]
+
+        NODATA_value = linecache.getline('{}.{}'.format(file_name,file_type),6).split(' ')
+        NODATA_value = NODATA_value[1]
+
+        results = []
+
+        print("Inserting data..")
+
+        for i in tqdm(range(7,n_row+7)):
+
+            each_line = linecache.getline('{}.{}'.format(file_name,file_type),i).split(' ')
+            results.append(list(map(int, each_line)))
+
+        print("Creating CSV..")
+
+        data_frame = pd.DataFrame(results)
+        data_frame.to_csv(file, header=False, index=False)
+
+        location = os.path.abspath('{}.{}'.format(file_name,convert_to))
+
+        print("Successful. PATH: {}\n".format(location))
+
+        file.close()
+
+    '''Interative for installing for all .asc files'''
+
+    def process_grid():
+
+        files = get_grid_files()
+        for file in files:
+            install_grid(file)
